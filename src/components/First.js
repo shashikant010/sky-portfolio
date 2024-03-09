f1db504efe9d1d784eb07332e7399af4a53fb3b0
shashikant010 <shashikantyadav0503@gmail.com>
2024-03-09 22:27:20 +0530

    initial commit with some basic of first page

---
+++ src/components/First.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function First() {
+  return (
+    <div className="first-page">
+        <div className="container">
+        <div className="welcomeMsg">
+                    <h2>Welcome to Sky World</h2>
+                </div>
+        </div>
+       
+    </div>
+  )
+}
